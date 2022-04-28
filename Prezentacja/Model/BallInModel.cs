07390f72874dbb3695a1260e705b01d6d903df8d
Piggey <49120531+Piggey@users.noreply.github.com>
2022-04-28 19:24:22 +0200

    warstwa `Prezentacja.Model`

---
+++ Prezentacja/Model/BallInModel.cs
@@ -0,0 +1,23 @@
+﻿using Logika;
+
+namespace Model
+{
+
+    public class BallInModel
+    {
+        private const int VIEW_WIDTH = 854;
+        private const int VIEW_HEIGHT = 480;
+        
+        private Ball ball;
+
+        public BallInModel(Ball ball)
+        {
+            this.ball = ball;
+        }
+
+        public double PositionX => (ball.x * VIEW_WIDTH) / Board.WIDTH;
+
+        public double PositionY => (ball.y * VIEW_HEIGHT) / Board.HEIGHT;
+
+    }
+}
---
+++ Prezentacja/Model/ModelAPI.cs
@@ -0,0 +1,47 @@
+﻿using Logika;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public abstract class ModelAPI
+    {
+        public abstract List<BallInModel> Balls { get; }
+        public abstract void AddBallsAndStart(int ballsAmount);
+
+        public static ModelAPI CreateApi()
+        {
+            return new ModelBall();
+        }
+
+        internal class ModelBall : ModelAPI
+        {
+            private Board board;
+            public override List<BallInModel> Balls => ChangeBallToBallInModel();
+
+            public ModelBall()
+            {
+                this.board = new Board();
+            }
+
+            public override void AddBallsAndStart(int ballsAmount)
+            {
+                for (int i = 0; i < ballsAmount; i++)
+                    board.AddBall();
+
+                board.RunSimulation();
+            }
+
+            public List<BallInModel> ChangeBallToBallInModel()
+            {
+                List<BallInModel> result = new List<BallInModel>();
+
+                foreach (Ball ball in board.balls)
+                {
+                    result.Add(new BallInModel(ball));
+                }
+                return result;
+            }
+        }
+
+    }
+}
