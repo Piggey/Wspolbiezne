676a469142743016e709088b9ecbe9334553f0bb
Piggey <49120531+Piggey@users.noreply.github.com>
2022-05-16 22:58:34 +0200

    move `Ball` and `Board` to `Data` layer

---
+++ Dane/Ball.cs
@@ -0,0 +1,71 @@
+﻿using System.Numerics;
+
+namespace Dane
+{
+    public class Ball
+    {
+        private const float SpeedMin = 0.2f;
+        private const float SpeedMax = 1.0f;
+        private const float RadiusMin = 20.0f;
+        private const float RadiusMax = 50.0f;
+        private const float MassMin = 1.0f;
+        private const float MassMax = 5.0f;
+
+        public int Id { get; }
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public float Radius { get; private set; }
+        public float Mass { get; private set; }
+        
+        public Ball(int id)
+        {
+            Random r = new Random();
+
+            Id = id;
+
+            Position = new Vector2(
+                r.NextSingle() * Board.Width,
+                r.NextSingle() * Board.Height
+            );
+
+            Velocity = new Vector2(
+                r.NextSingle() * (SpeedMax - SpeedMin) + SpeedMin,
+                r.NextSingle() * (SpeedMax - SpeedMin) + SpeedMin
+            );
+
+            Radius = r.NextSingle() * (RadiusMax - RadiusMin) + RadiusMin;
+            Mass = r.NextSingle() * (MassMax - MassMin) + MassMin;
+            
+            Console.WriteLine($"Ball: new Ball created. [ id: {Id}, " +
+            $"position: {Position}, velocity: {Velocity}, radius: {Radius}, mass: {Mass} ]");
+        }
+
+        public void Move()
+        {
+            Position = Vector2.Add(Position, Velocity);
+            if (Position.X <= 0)
+            {
+                Position = Position with { X = 0, Y = Position.Y };
+                Velocity = Velocity with { X = -Velocity.X, Y = Velocity.Y };
+            }
+
+            if (Position.X >= Board.Width)
+            {
+                Position = Position with { X = Board.Width, Y = Position.Y };
+                Velocity = Velocity with { X = -Velocity.X, Y = Velocity.Y };
+            }
+
+            if (Position.Y <= 0)
+            {
+                Position = Position with { X = Position.X, Y = 0 };
+                Velocity = Velocity with { X = Velocity.X, Y = -Velocity.Y };
+            }
+
+            if (Position.Y >= Board.Height)
+            {
+                Position = Position with { X = Position.X, Y = Board.Height };
+                Velocity = Velocity with { X = Velocity.X, Y = -Velocity.Y };
+            }
+        }
+    }
+}
---
+++ Dane/Board.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Dane
+{
+    public class Board : DataApi
+    {
+        // keep the ratio 16:9
+        public const int Width = 160;
+        public const int Height = 90;
+        
+        public List<Task> BallTasks { get; private set; }
+        public List<Ball> Balls { get; set; }
+        public int Size { get; private set; }
+
+        public Board()
+        {
+            BallTasks = new List<Task>();
+            Balls = new List<Ball>();
+            Size = Balls.Count;
+        }
+
+        /// <summary>
+        /// Get current count of created Ball Tasks
+        /// </summary>
+        /// <returns>Task count</returns>
+        public override int GetBallCount()
+        {
+            return Size;
+        }
+
+        /// <summary>
+        /// Start new Task running a single Ball in loop
+        /// </summary>
+        public override void AddBall()
+        {
+            Ball b = new Ball(Size);
+            Task t = new Task(b.MoveLoop);
+            BallTasks.Add(t);
+            Balls.Add(b);
+            t.Start();
+            Console.WriteLine($"Board: Task #{Size} created.");
+            Size++;
+        }
+
+        /// <summary>
+        /// Kills newest Task of a Ball in loop created
+        /// </summary>
+        public override void RemoveBall()
+        {
+            if (Size == 0)
+                return;
+            
+            Balls[Size - 1].Token.can
+            
+            BallTasks.RemoveAt(Size - 1);
+            Console.WriteLine($"Board: Task #{Size} ended.");
+            Size--;
+        }
+    }
+}
